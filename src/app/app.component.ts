ca270ee68f0c65940ba295b483dea25c8b2231ba
rony thankachan <ronythankachan@ronys-MacBook-Air.local>
2021-10-10 23:50:42 +0530

    recommendation ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,109 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test';
+  
+  recommendationTypes:string[]= ['Select','Topic','Feature']
+  topicList:string[] = ['Loan','Banking'];
+  selectedTopic:string='';
+  featureList:string[] = ['interest','tenure']
+
+  ruleList:any=[
+    {
+      "id":"1",
+      "recommendation_type":"feature",
+      "topic_predicted":{
+        "uri":"CONC876",
+        "forDisplay":"Loan"
+      },
+      "feature_predicted":{
+        "uri":"PROP123",
+        "forDisplay":"Interest rate"
+      },
+      "recommendation_list":[
+        {
+          "uri":"PROP4546",
+          "forDisplay":"Tenure period"
+        },
+        {
+          "uri":"PROP4543",
+          "forDisplay":"Apply for loan"
+        }
+      ]
+    },
+    {
+      "id":"2",
+      "recommendation_type":"topic",
+      "topic_predicted":{
+        "uri":"CONC123",
+        "forDisplay":"Loan"
+      },
+      "feature_predicted":{
+        "uri":"PROP123",
+        "forDisplay":"Calculate EMI"
+      },
+      "recommendation_list":[
+        {
+          "uri":"CONC9879",
+          "forDisplay":"House loans"
+        },
+        {
+          "uri":"CONC65794",
+          "forDisplay":"Student Loans"
+        },
+        {
+          "uri":"CONC9879",
+          "forDisplay":"House loans"
+        },
+        {
+          "uri":"CONC65794",
+          "forDisplay":"Student Loans"
+        },
+        {
+          "uri":"CONC9879",
+          "forDisplay":"House loans"
+        },
+        {
+          "uri":"CONC65794",
+          "forDisplay":"Student Loans"
+        },
+        {
+          "uri":"CONC9879",
+          "forDisplay":"House loans"
+        },
+        {
+          "uri":"CONC65794",
+          "forDisplay":"Student Loans"
+        },
+        {
+          "uri":"CONC9879",
+          "forDisplay":"House loans"
+        },
+        {
+          "uri":"CONC65794",
+          "forDisplay":"Student Loans"
+        }
+      ]
+    }
+  ];
+  selectedRecommendationType:string='Select';
+  addRule:boolean =false;
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+  
+  changeRecommendationType(recommendationType:string):void{
+    this.selectedRecommendationType = recommendationType;
+  }
+
+  enableAddRule(){
+    this.addRule = true;
+  }
+  deleteRuleForm(){
+    this.addRule = false;
+  }
+  deleteRule(id:String){
+    this.ruleList = this.ruleList.filter((rule: any) => rule.id !== id)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnotherComponent } from './another/another.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnotherComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
